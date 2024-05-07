c1a5aa53c91d92e2295d9bbc3f014e08e8d03c2a
coledie <colesteronid@gmail.com>
2024-05-06 22:49:10 -0500

    Slow.

--- sudocube.py
+++ sudocube.py
@@ -29,125 +29,125 @@ class Cube(Shape):
     def _init_board(self):
         # Section 1
         section = self.shape[0]
-        section[3, 0] = 4
-        section[4, 0] = 7
-        section[8, 0] = 1
-        section[3, 1] = 1
-        section[5, 1] = 8
-        section[0, 2] = 3
-        section[1, 2] = 5
-        section[7, 2] = 7
-        section[5, 4] = 9
-        section[7, 4] = 1
-        section[6, 5] = 7
-        section[8, 5] = 4
-        section[1, 6] = 6
-        section[3, 6] = 3
-        section[1, 7] = 7
-        section[2, 7] = 3
-        section[6, 8] = 6
+        section[3, 0] = 3
+        section[4, 0] = 6
+        section[8, 0] = 0
+        section[3, 1] = 0
+        section[5, 1] = 7
+        section[0, 2] = 2
+        section[1, 2] = 4
+        section[7, 2] = 6
+        section[5, 4] = 8
+        section[7, 4] = 0
+        section[6, 5] = 6
+        section[8, 5] = 3
+        section[1, 6] = 5
+        section[3, 6] = 2
+        section[1, 7] = 6
+        section[2, 7] = 2
+        section[6, 8] = 5
 
         # Section 2
         section = self.shape[1]
-        section[6, 0] = 6
-        section[2, 1] = 4
-        section[3, 1] = 9
-        section[5, 1] = 6
-        section[4, 2] = 2
-        section[2, 3] = 7
-        section[3, 3] = 4
-        section[1, 4] = 2
-        section[4, 4] = 3
-        section[5, 4] = 8
-        section[5, 5] = 5
-        section[1, 6] = 9
-        section[6, 6] = 7
-        section[0, 7] = 3
-        section[7, 7] = 4
-        section[1, 8] = 7
-        section[6, 8] = 8
+        section[6, 0] = 5
+        section[2, 1] = 3
+        section[3, 1] = 8
+        section[5, 1] = 5
+        section[4, 2] = 1
+        section[2, 3] = 6
+        section[3, 3] = 3
+        section[1, 4] = 1
+        section[4, 4] = 2
+        section[5, 4] = 7
+        section[5, 5] = 4
+        section[1, 6] = 8
+        section[6, 6] = 6
+        section[0, 7] = 2
+        section[7, 7] = 3
+        section[1, 8] = 6
+        section[6, 8] = 7
 
         # Section 3
         section = self.shape[2]
-        section[1, 0] = 7
-        section[6, 0] = 8
-        section[0, 1] = 5
-        section[7, 1] = 6
-        section[7, 2] = 9
-        section[2, 3] = 8
-        section[6, 4] = 5
-        section[7, 4] = 2
-        section[3, 5] = 5
-        section[6, 5] = 7
-        section[3, 6] = 6
-        section[4, 6] = 5
-        section[6, 6] = 3
-        section[0, 8] = 9
-        section[6, 8] = 6
+        section[1, 0] = 6
+        section[6, 0] = 7
+        section[0, 1] = 4
+        section[7, 1] = 5
+        section[7, 2] = 8
+        section[2, 3] = 7
+        section[6, 4] = 4
+        section[7, 4] = 1
+        section[3, 5] = 4
+        section[6, 5] = 6
+        section[3, 6] = 5
+        section[4, 6] = 4
+        section[6, 6] = 2
+        section[0, 8] = 8
+        section[6, 8] = 5
 
         # Section 4
         section = self.shape[3]
-        section[7, 0] = 3
-        section[8, 1] = 5
-        section[1, 2] = 2
-        section[2, 2] = 3
-        section[4, 2] = 8
-        section[3, 3] = 4
-        section[4, 3] = 3
-        section[2, 4] = 8
-        section[4, 4] = 9
-        section[6, 4] = 3
-        section[2, 5] = 4
-        section[5, 5] = 7
-        section[7, 5] = 2
-        section[0, 6] = 3
-        section[1, 6] = 6
-        section[5, 6] = 4
-        section[7, 6] = 7
-        section[6, 7] = 1
-        section[3, 8] = 5
-        section[6, 8] = 2
-        section[8, 8] = 9
+        section[7, 0] = 2
+        section[8, 1] = 4
+        section[1, 2] = 1
+        section[2, 2] = 2
+        section[4, 2] = 7
+        section[3, 3] = 3
+        section[4, 3] = 2
+        section[2, 4] = 7
+        section[4, 4] = 8
+        section[6, 4] = 2
+        section[2, 5] = 3
+        section[5, 5] = 6
+        section[7, 5] = 1
+        section[0, 6] = 2
+        section[1, 6] = 5
+        section[5, 6] = 3
+        section[7, 6] = 6
+        section[6, 7] = 0
+        section[3, 8] = 4
+        section[6, 8] = 1
+        section[8, 8] = 8
 
         # Section 5
         section = self.shape[4]
-        section[1, 2] = 8
-        section[3, 2] = 2
-        section[5, 3] = 3
-        section[6, 3] = 1
-        section[8, 3] = 4
-        section[2, 5] = 7
-        section[5, 5] = 8
-        section[1, 6] = 6
-        section[3, 7] = 5
-        section[4, 7] = 2
-        section[3, 8] = 4
-        section[5, 8] = 6
-        section[8, 8] = 1
+        section[1, 2] = 7
+        section[3, 2] = 1
+        section[5, 3] = 2
+        section[6, 3] = 0
+        section[8, 3] = 3
+        section[2, 5] = 6
+        section[5, 5] = 7
+        section[1, 6] = 5
+        section[3, 7] = 4
+        section[4, 7] = 1
+        section[3, 8] = 3
+        section[5, 8] = 5
+        section[8, 8] = 0
 
         # Section 6
         section = self.shape[5]
-        section[0, 0] = 9
-        section[6, 0] = 6
-        section[1, 1] = 5
-        section[0, 2] = 2
-        section[1, 3] = 8
-        section[3, 3] = 7
-        section[7, 3] = 1
-        section[8, 3] = 4
-        section[3, 4] = 5
-        section[6, 4] = 3
-        section[0, 5] = 5
-        section[3, 5] = 8
-        section[8, 5] = 6
-        section[4, 6] = 8
-        section[5, 6] = 6
-        section[7, 6] = 9
-        section[2, 7] = 5
-        section[6, 7] = 8
-        section[3, 8] = 4
-        section[4, 8] = 7
-        section[8, 8] = 1
+        section[0, 0] = 8
+        section[6, 0] = 5
+        section[1, 1] = 4
+        section[0, 2] = 1
+        section[1, 3] = 7
+        section[3, 3] = 6
+        section[7, 3] = 0
+        section[8, 3] = 3
+        section[3, 4] = 4
+        section[6, 4] = 2
+        section[0, 5] = 4
+        section[3, 5] = 7
+        section[8, 5] = 5
+        section[4, 6] = 7
+        section[5, 6] = 5
+        section[7, 6] = 8
+        section[2, 7] = 4
+        section[6, 7] = 7
+        section[3, 8] = 3
+        section[4, 8] = 6
+        section[8, 8] = 0
 
     def print(self):
         null = np.full((self.size, self.size), -1, dtype=self.shape[0].dtype)
@@ -187,27 +187,10 @@ def isin(v, arr):
             return True
     return False
 
-def _is_valid(shape, section, x, y, v):
-    if isin(v, shape.shape[section][:, x]):
-        return False
-    if isin(v, shape.shape[section][y, :]):
-        return False
 
-    return True
-
-    first = 0
-    last = shape.size - 1
-    connection, newx, newy = shape.get_connection(section, x, y)
-    if y in [first, last] and v in connection[y, :]:
-        return False
-    if x in [first, last] and v in connection[:, x]:
-        return False
-    return True
-
-
-def _solve_backtrack(shape: Shape, section: int, x: int, y: int) -> bool:
+def _solve_backtrack(shape: Shape, section: int, x: int, y: int, data: list) -> bool:
     if x == shape.size:
-        return _solve_backtrack(shape, section, 0, y + 1)
+        return _solve_backtrack(shape, section, 0, y + 1, data)
     if y == shape.size:
         return True
     if x < 0 or y < 0 or x > shape.size or y > shape.size:
@@ -217,24 +200,51 @@ def _solve_backtrack(shape: Shape, section: int, x: int, y: int) -> bool:
     if original_value == -1:
         return True
     if original_value != -2:
-        return _solve_backtrack(shape, section, x + 1, y)
-    for v in range(shape.size):
-        if not _is_valid(shape, section, x, y, v):
-            continue
+        return _solve_backtrack(shape, section, x + 1, y, data)
 
+    for v in data[0][x]:
+        if v not in data[1][y]:
+            continue
+        '''
+        first = 0
+        last = shape.size - 1
+        connection, newx, newy = shape.get_connection(section, x, y)
+        if y in [first, last] and v in connection[y, :]:
+            return False
+        if x in [first, last] and v in connection[:, x]:
+            return False
+        return True
+        '''
+        data[0][x].remove(v)
+        data[1][y].remove(v)
         shape.update(section, (x, y), v)
-        res = _solve_backtrack(shape, section, x + 1, y)
+        res = _solve_backtrack(shape, section, x + 1, y, data)
         if res:
             return True
+        else:
+            data[0][x].add(v)
+            data[1][y].add(v)
 
     shape.update(section, (x, y), -2)
     return False
 
 
 def solve(shape: Shape):
+    time_start = time.time()
     for section in range(len(shape.shape)):
-        res = _solve_backtrack(shape, section, 0, 0)
-        print(f"Section {section}: {res}")
+        data = (
+            [{i for i in range(shape.size)} for _ in range(shape.size)],
+            [{i for i in range(shape.size)} for _ in range(shape.size)]
+        )
+        for y, row in enumerate(shape.shape[section]):
+            for x, v in enumerate(row):
+                if v >= 0:
+                    print(x, y, v)
+                    data[0][x].remove(v)
+                    data[1][y].remove(v)
+
+        res = _solve_backtrack(shape, section, 0, 0, data)
+        print(f"Section {section}: {res} {time.time() - time_start}")
     return shape
 
 
@@ -243,7 +253,6 @@ if __name__ == '__main__':
     cube.print()
 
     try:
-        # TODO test sudokube solutions in game!!!!!!
         time_start = time.time()
         sol = solve(cube)
         print()
@@ -251,5 +260,5 @@ if __name__ == '__main__':
         print()
         sol.print()
         print(time.time() - time_start)
-    except Exception:
+    except KeyboardInterrupt:
         pass
